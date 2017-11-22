0e76f30a1cf679c85eed7cc67960d439b97e690f
mariush2 <MariusTobi_98@hotmail.com>
2017-11-22 20:32:56 +0100

    Done 40

--- problems36_40.py
+++ problems36_40.py
@@ -3,7 +3,9 @@ from problems1_5 import palindrome as palindrome
 #Problem 37
 from problems31_35 import isPrime as isPrime
 from itertools import *
-
+#Problem 38
+from problems31_35 import pandigital as pandigital
+from math import *
 
 #Problem 36
 def double_base_palindrome_sum(upper):
@@ -39,9 +41,89 @@ def all_truncatable_primes():
 
 
 #Problem 38
+def concate(num, num2):
+    return int(str(num) + str(num2))
+
+def pandigital_multiple():
+    highest = 0
+    for i in range(9234, 9387):
+        current = concate(i, i*2)
+        if(pandigital(current) and current > highest):
+            highest = current
+    return highest
+
+
+#Problem 39
+#Right angle triangles only!
+#a^2 + b^2 = c^2
+def find_amount_of_solutions(p):
+    amount = 0
+    for a in xrange(2, p/2):
+        for b in xrange(2, a):
+            c = sqrt(a ** 2 + b ** 2)
+            if((a ** 2 + b ** 2) == c ** 2 and a+b+c == p):
+                amount +=1
+    return amount
+
+
+def find_best_p(upper):
+    current_p = 1
+    best_p = 0
+    best_amount = 0
+    while(current_p <= upper):
+        amount = find_amount_of_solutions(current_p)
+        if(amount > best_amount):
+            best_p = current_p
+            best_amount = amount
+        current_p += 1
+    return best_p, best_amount
+
+
+#Problem 40
+def d_n(n, c):
+    return int(c[n-1])
+
+
+def c_n_length(n):
+    full = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    c = "123456789"
+    current = 1
+    passed = False
+    while len(c) <= n + 1:
+        if(current < 10):
+            if(passed):
+                for i in xrange(10):
+                    c += full[i] + full[current]
+            else:
+                for i in xrange(10):
+                    c += full[current] + full[i]
+            current += 1
+        else:
+            passed = True
+            current = 0
+    return c
+
+
+def problem_40(upper):
+    product = 1
+    c = c_n_length(10 ** upper)
+    for i in xrange(upper):
+        cur = d_n(10 ** i, c)
+        product *= cur
+    return product
+
 
 #Problem 36
 #double_base_palindrome_sum(1000000)
 #
 #Problem 37
 #all_truncatable_primes()
+#
+#Problem 38
+#pandigital_multiple()
+#
+#Problem 39
+#find_best_p(1000)
+#
+#Problem 40
+problem_40(7)
