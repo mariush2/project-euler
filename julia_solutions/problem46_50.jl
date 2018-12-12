2da692a25994ba33d7a59176c1aee3b519f56c0b
Marius Tobiassen <MariusTobi_98@hotmail.com>
2018-12-12 16:49:27 +0100

    Done 49

--- julia_solutions/problem46_50.jl
+++ julia_solutions/problem46_50.jl
@@ -1,4 +1,4 @@
-using Primes
+using Primes, Combinatorics
 # Problem 46
 function istwicesquare(number)
     squareTest = sqrt(number/2)
@@ -78,4 +78,67 @@ function problem48()
     return string(sum([i^i for i in 1:big(1000)]))[end-9:end]
 end
 
-println(@time problem48())
+#println(@time problem48())
+
+#Problem 49
+function findpermutationprime!(n, all)
+    for num in permutations(string(n))
+        if num[1] != '0'
+            num = parse(Int64, join(num))
+            if isprime(num) && !(num in all)
+                #Found second, now save diff and check if the last is prime
+                push!(all, num)
+            end
+        end
+    end
+end
+
+function checkdiff!(A)
+    sort!(A)
+    for i = 1:length(A)
+        for j = i:length(A)
+            if i != j
+                diff = abs(A[j] - A[i])
+            else
+                diff = Inf
+            end
+            if A[j] > A[i]
+                if (A[j] + diff) in A
+                    #Found the third prime
+                    return [A[i], A[j], A[j] + diff]
+                end
+            elseif A[i] > A[j]
+                if (A[i] + diff) in A
+                    #Found the third prime
+                    return [A[j], A[i], A[i] + diff]
+                end
+            end
+
+        end
+    end
+    return []
+end
+
+function problem49()
+    n = 1001
+    forbidden = []
+    findpermutationprime!(1487, forbidden)
+    while n < 10000
+        all = []
+        if isprime(n) && !(n in forbidden)
+            #Found first, now look for second
+            push!(all, n)
+            findpermutationprime!(n, all)
+            all = checkdiff!(all)
+            if length(all) == 3
+                #Found
+                println(all)
+                return join(all)
+            end
+        end
+        n = n + 1
+    end
+    return "Not found"
+end
+
+println(@time problem49())
